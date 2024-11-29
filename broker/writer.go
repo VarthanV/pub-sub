3f3f478930569311507de8d2f9a51e8437a63729
VarthanV <vishnulatha006@gmail.com>
2024-11-29 18:56:11 +0530

    fixes

--- broker/writer.go
+++ broker/writer.go
@@ -5,7 +5,8 @@ import (
 )
 
 type writer struct {
-	bufferSize int // buffer size corresponds to item length in the array
+	// buffer size corresponds to item length in the array
+	bufferSize int
 	buffer     []interface{}
 	db         *gorm.DB
 }
@@ -20,6 +21,7 @@ func NewWriter(db *gorm.DB, size int) *writer {
 func (w *writer) Write(val interface{}) (n int, err error) {
 	w.buffer = append(w.buffer, val)
 	if len(w.buffer) >= w.bufferSize {
+		// flush
 		tx := w.db.CreateInBatches(w.buffer, len(w.buffer))
 		if tx.Error != nil {
 			return 0, tx.Error
